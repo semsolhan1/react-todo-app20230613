b7b8d852abf676f9971ff204481ceb608bbd64f9
KOOBONSIK <megatron1234@naver.com>
2023-07-05 14:04:22 +0900

    # 18. 프로필 이미지 처리 - AWS S3 수정했지만 안됌

--- src/component/layout/Header.js
+++ src/component/layout/Header.js
@@ -1,13 +1,12 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react';
 import {AppBar, Toolbar, Grid, 
     Typography, Button} from "@mui/material";
 import './header.css';
 import { Link, useNavigate } from 'react-router-dom';
 
-import { isLogin, getLoginUserInfo } from '../../util/Login-utils';
-import AuthContext from '../../util/Authcontext';
+import { isLogin, getLoginUserInfo } from '../../util/login-utils';
+import AuthContext from '../../util/AuthContext';
 import { API_BASE_URL, USER } from '../../config/host-config';
-
 
 const Header = () => {
 
@@ -15,55 +14,52 @@ const Header = () => {
 
     const redirection = useNavigate();
 
-    
-
     //프로필 이미지 url 상태 변수
     const [profileUrl, setProfileUrl] = useState(null);
 
     // AuthContext에서 로그인 상태와 onLogout 함수를 가져옵니다.
     const {isLoggedIn, onLogout, userName} = useContext(AuthContext);
 
-//  로그아웃 핸들러
-const logoutHandler = e => {
-    // AuthContext의 onLogout 함수를 호출하여 로그인 상태를 업데이트한다.
-    onLogout();
-    redirection('/login');
-}
-
-const fetchProfileImage = async() => {
-    const res = await fetch(profileRequestURL, {
-        method: 'GET',
-        headers: { 'Authorization' : 'Bearer ' + localStorage.getItem('ACCESS_TOKEN')}
-    });
-
-    if(res.status === 200) {
-        //서버에서는 이제 s3 url이 응답된다.
-        const imgUrl = await res.text();
-        setProfileUrl(imgUrl);
-
-
-        /*
-        //서버에서는 직렬화된 이미지가 응답된다.
-        const profileBlob = await res.blob();
-        //해당 이미지를  imgUrl로 변경
-        const imgUrl = window.URL.createObjectURL(profileBlob);
-        setProfileUrl(imgUrl);
-        */
-    } else { 
-        const err = await res.text();
-        setProfileUrl(null);
+    //로그아웃 핸들러
+    const logoutHandler = e => {
+        // AuthContext의 onLogout 함수를 호출하여 로그인 상태를 업데이트합니다.
+        onLogout();
+        redirection('/login');
     }
 
-}
+    const fetchProfileImage = async() => {
+        const res = await fetch(profileRequestURL, {
+            method: 'GET',
+            headers: { 'Authorization' : 'Bearer ' +  localStorage.getItem('ACCESS_TOKEN')}
+        });
 
-useEffect(() => {
-
-    fetchProfileImage();
-
-}, [isLoggedIn]);
-
+        if (res.status === 200) {
+            // 서버에서는 이제 s3 url이 응답된다.
+            const imgUrl = await res.text();
+            setProfileUrl(imgUrl);
 
 
+            /*
+            // 서버에서는 직렬화된 이미지가 응답된다.
+            const profileBlob = await res.blob();
+            // 해당 이미지를 imgUrl로 변경
+            const imgUrl = window.URL.createObjectURL(profileBlob);
+            setProfileUrl(imgUrl);
+            */
+        } else {
+            const err = await res.text();
+            setProfileUrl(null);
+        }
+
+    }
+
+    useEffect(() => {
+
+        fetchProfileImage();
+
+    }, [isLoggedIn]);
+ 
+  
     return (
         <AppBar position="fixed" style={{
             background: '#38d9a9',
@@ -82,32 +78,33 @@ useEffect(() => {
                                 {
                                     isLoggedIn
                                     ? userName + '님'
-                                    :'오늘'
+                                    : '오늘'
                                 }
                                 의 할일
-                            </Typography>   
-                            {isLoggedIn &&
+                            </Typography>
+                            {isLoggedIn && 
                             <img 
-                            src={profileUrl || require('../../assets/img/anonymous.jpg')}
-                            alt='프사프사'
-                            style={{
-                                marginLeft: 20,
-                                width: 75,
-                                height: 75,
-                                borderRadius: '50%'
-                            }}
+                                src={profileUrl || require('../../assets/img/anonymous.jpg')}
+                                alt='프사프사'
+                                style={{
+                                    marginLeft: 20,
+                                    width: 75,
+                                    height: 75,
+                                    borderRadius: '50%'
+                                }}
                             />
-                        }
+                            } 
                         </div>
                     </Grid>
-    
+                    
                     <Grid item>
                         <div className='btn-group'>
-                          {isLogin()
+                        {isLogin()
                                 ?
                                 (
-                                    <button className='logout-btn'
-                                    onClick={logoutHandler}>
+                                    <button 
+                                        className='logout-btn'
+                                        onClick={logoutHandler}>
                                         로그아웃
                                     </button>
                                 )
@@ -119,14 +116,16 @@ useEffect(() => {
                                     </>
                                 )
 
-                          }
+                        }
                         </div>
                     </Grid>
+                    
     
                 </Grid>
             </Toolbar>
         </AppBar>
       );
-                    }
+
+}
 
 export default Header
--- src/util/Authcontext.js
+++ src/util/Authcontext.js
@@ -6,49 +6,50 @@ const AuthContext = React.createContext({
     isLoggedIn: false, //로그인 했는지의 여부 추적
     userName: '',
     onLogout: () => {}, //더미 함수를 넣으면 자동완성 시 편함.
-    onLogin: (email, pssword) => {},
+    onLogin: (email, password) => {},
     setUserInfo: () => {}
 });
 
-//위에서 생성한 Context를제공할 수 있는 Provider
-//이 컴포넌트를 통해 자식 컴포넌트들에게 인증 상태와 관련된 함수들을 전달할 수 있음.
+// 위에서 생성한 Context를 제공할 수 있는 provider
+// 이 컴포넌트를 통해 자식 컴포넌트들에게 인증 상태와 관련된 함수들을 전달할 수 있음.
 export const AuthContextProvider = props => {
+
+    console.log('AuthContextProvider Rendering!');
 
     const [isLoggedIn, setIsLoggedIn] = useState(false);
     const [userName, setUserName] = useState('');
 
-    //컴포넌트가 랜더링 될 때 localStorage에서 로그인 정보를 가지고 와서 상태를 설정.
+    //컴포넌트가 렌더링 될 때 localStorage에서 로그인 정보를 가지고 와서 상태를 설정.
     useEffect(() => {
         if(localStorage.getItem('isLoggedIn') === '1') {
             setIsLoggedIn(true);
             setUserName(localStorage.getItem('LOGIN_USERNAME'));
-        }
+        } 
     }, []);
+
 
     //로그아웃 핸들러
     const logoutHandler = () => {
         localStorage.clear();
         setIsLoggedIn(false);
-    }
+    };
 
     //로그인 핸들러
     const loginHandler = (token, userName, role) => {
-
+        localStorage.setItem('isLoggedIn', '1');
         //json에 담긴 인증정보를 클라이언트에 보관
-        //1. 로컬 스토리지 - 브라우저가 종료되어도 보관됨.
-        //2. 세션 스토리지 - 브라우저가 종료되면 사라짐.
+        // 1. 로컬 스토리지 - 브라우저가 종료되어도 보관됨.
+        // 2. 세션 스토리지 - 브라우저가 종료되면 사라짐.
         //sessionStorage.setItem('', );
         //localStorage.setItem('저장할 데이터 이름', 저장할 데이터의 값);
         localStorage.setItem('ACCESS_TOKEN', token);
         localStorage.setItem('LOGIN_USERNAME', userName);
         localStorage.setItem('USER_ROLE', role);
-
-        localStorage.setItem("isLoggedIn", "1");
         setIsLoggedIn(true);
         setUserName(userName);
     };
 
-    // 토큰 및 로그인 유저 데이터를 브라우저에 저장하는 함수
+    //토큰 및 로그인 유저 데이터를 브라우저에 저장하는 함수
     const setLoginUserInfo = ({ token, userName, role }) => {
         localStorage.setItem('ACCESS_TOKEN', token);
         localStorage.setItem('LOGIN_USERNAME', userName);
@@ -56,7 +57,6 @@ export const AuthContextProvider = props => {
     }
 
     return (
-        // 데이터를 전달하고자 하는 자식 컴포넌트들을 Provider로 감쌉니다.
         <AuthContext.Provider value={{
             isLoggedIn, //변수이름과 프로퍼티 값이 같으면 생략
             userName,
@@ -64,13 +64,12 @@ export const AuthContextProvider = props => {
             onLogin: loginHandler,
             setUserInfo: setLoginUserInfo
         }}>
-            {props.children} {/* 자식 컴포넌트들 */}
+            {props.children}
         </AuthContext.Provider>
     );
 
 };
 
 export default AuthContext;
-
 
 
